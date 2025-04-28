b2a8e6564502147e6099c4d2ffeffcfe10349a9f
Taggart Smith <taggart.smith@gmail.com>
2025-04-28 17:22:17 -0600

    feat: Initialize Angular client project with dependencies
    - Set up Angular v19.2 project structure
    - Add Angular Material and CDK dependencies
    - Configure basic development environment with testing tools
    - Include essential Angular core modules and dependencies

---
+++ client/src/app/components/search-movie/search-movie.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-movie',
+  imports: [],
+  templateUrl: './search-movie.component.html',
+  styleUrl: './search-movie.component.css'
+})
+export class SearchMovieComponent {
+
+}
