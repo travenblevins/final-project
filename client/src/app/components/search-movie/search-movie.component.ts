615649097acec459826513ffeac30805457fb5a2
Taggart Smith <taggart.smith@gmail.com>
2025-04-29 09:49:49 -0600

    Adjusted Styles for navbar and Search Movies page.

--- client/src/app/components/search-movie/search-movie.component.ts
+++ client/src/app/components/search-movie/search-movie.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-search-movie',
-  imports: [],
+  imports: [MatFormFieldModule, FormsModule, CommonModule, MatInputModule, MatCardModule],
   templateUrl: './search-movie.component.html',
   styleUrl: './search-movie.component.css'
 })
 export class SearchMovieComponent {
+  searchText: string = '';
 
+  clearSearch() {
+    this.searchText = '';
+  }
 }
